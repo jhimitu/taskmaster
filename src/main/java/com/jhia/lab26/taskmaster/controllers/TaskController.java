a15c0361b3f3982f528628c7a0a548550f287fa9
Jhia Turner <jhimitu@gmail.com>
2019-07-03 11:44:12 -0700

    set up task controller with GET route for all tasks and POST route for adding one task

--- src/main/java/com/jhia/lab26/taskmaster/controllers/TaskController.java
+++ src/main/java/com/jhia/lab26/taskmaster/controllers/TaskController.java
@@ -1,4 +1,27 @@
 package com.jhia.lab26.taskmaster.controllers;
 
+import com.jhia.lab26.taskmaster.models.Task;
+import com.jhia.lab26.taskmaster.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class TaskController {
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return (List) taskRepository.findAll();
+    }
+
+    @PatchMapping("/tasks/{id}/status")
+    public Task patchStatus(@PathVariable String id) {
+        Task task = taskRepository.findById(id).get();
+//        task.toggleCompletedStatus();
+        taskRepository.save(task);
+        return task;
+    }
 }
--- src/main/java/com/jhia/lab26/taskmaster/repositories/TaskRepository.java
+++ src/main/java/com/jhia/lab26/taskmaster/repositories/TaskRepository.java
@@ -3,5 +3,8 @@ package com.jhia.lab26.taskmaster.repositories;
 import com.jhia.lab26.taskmaster.models.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends CrudRepository<Task, String> {
+    Optional<Task> findById(String id);
 }
